22380c2c56341eb96c59544f543b4f5d3bacfb4e
Elliott Davis <edavis@chef.io>
2016-09-02 16:24:35 -0500

    [IPO-380] Update to add ssl toggle
    * provide way to send messages to automate instances with self-signed certs

--- habitat/config/habistone.rb
+++ habitat/config/habistone.rb
@@ -3,3 +3,4 @@ data_collector_token "{{cfg.data_collector_token}}"
 supervisor_host "{{cfg.supervisor_host}}"
 habitat_ring_id "{{cfg.habitat_ring_id}}"
 habitat_ring_alias "{{cfg.habitat_ring_alias}}"
+ssl_verification_enabled {{cfg.ssl_verification_enabled}}
