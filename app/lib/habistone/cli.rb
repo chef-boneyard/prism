557e245eabe6df5c107af336ad6bedc7e967fa8a
kmacgugan <kmacgugan@chef.io>
2016-09-13 12:34:53 -0700

    [IPO-369] Add configuration information to ring information
     * After retrieving the census, prism will retrieve the configuration from
    each supervisor listed in the census and add it to the ring information message.
     * bundler setup in bin/prism so we're using bundler properly
     * modified the habitat run hook to use `bundle exec`
     * enabling sync on $stderr in addition to $stdout
     * returning the output of `#body` on each call to RestClient.get
       to avoid the annoying/mysterious `#to_i` calls on the Response
       objects

--- app/lib/habistone/cli.rb
+++ app/lib/habistone/cli.rb
@@ -1,32 +1,32 @@
-require 'mixlib/cli'
+require "mixlib/cli"
 
 class Habistone
   class Cli
     include Mixlib::CLI
 
     option :config_file,
-           :short => '-c CONFIG',
-           :long  => '--config CONFIG',
-           :description => 'The path to the configuration file to use'
+           :short => "-c CONFIG",
+           :long  => "--config CONFIG",
+           :description => "The path to the configuration file to use"
 
     option :data_collector_url,
-           :long => '--data_collector_url URL',
-           :description => 'The data collector endpoint to use'
+           :long => "--data_collector_url URL",
+           :description => "The data collector endpoint to use"
 
     option :data_collector_token,
-           :long => '--data_collector_token TOKEN',
-           :description => 'The data collector token to auth with'
+           :long => "--data_collector_token TOKEN",
+           :description => "The data collector token to auth with"
 
     option :supervisor_host,
-           :long => '--supervisor_host HOST',
-           :description => 'Host that the supervisor is on, often localhost'
+           :long => "--supervisor_host HOST",
+           :description => "Host that the supervisor is on, often localhost"
 
     option :habitat_ring_id,
-           :long => '--habitat_ring_id UUID',
-           :description => 'The unique identifier of the habitat ring.'
+           :long => "--habitat_ring_id UUID",
+           :description => "The unique identifier of the habitat ring."
 
     option :habitat_ring_alias,
-           :long => '--habitat_ring_alias ALIAS',
-           :description => 'The alias of the habitat ring.'
+           :long => "--habitat_ring_alias ALIAS",
+           :description => "The alias of the habitat ring."
   end
 end
---
+++ app/lib/habistone/member_config.rb
@@ -0,0 +1,57 @@
+require "toml"
+
+class Habistone
+  class MemberConfig
+    attr_reader :supervisor_ip
+
+    def initialize(supervisor_ip)
+      @supervisor_ip = supervisor_ip
+    end
+
+    def get_config
+      begin
+        config_toml = RestClient.get("http://#{supervisor_ip}:9631/config").body
+        refract(config_toml)
+      rescue RestClient::ExceptionWithResponse => e
+        {
+          error: {
+            status: e.http_code,
+            message: e.response.body,
+          },
+        }
+      rescue => e
+        {
+          error: {
+            message: e.message,
+          },
+        }
+      end
+    end
+
+    def refract(config_toml)
+      parsed_toml = TOML::Parser.new(config_toml).parsed
+      {
+        bind: parsed_toml["bind"],
+        hab: parsed_toml["hab"],
+        cfg: parsed_toml["cfg"],
+        pkg: parsed_toml["pkg"]["ident"],
+        deps: project_deps(parsed_toml["pkg"]["deps"]),
+        sys: parsed_toml["sys"],
+      }
+    end
+
+    # Flatten deps tree into a package list
+    def project_deps(deps_tree)
+      deps_list = Set.new
+      project_deps_onto(deps_list, deps_tree)
+      deps_list.to_a
+    end
+
+    def project_deps_onto(deps_list, deps_tree)
+      deps_tree.each do |dep|
+        deps_list.add(dep["ident"])
+        project_deps_onto(deps_list, dep["deps"]) unless dep["deps"].nil? || dep["deps"].empty?
+      end
+    end
+  end
+end
